d3f917645305a3c9e690a509e2a3247788b88fdf
Gert Labuschagne <GertL@as-tech.co.za>
2016-05-19 12:48:34 +0200

    Updated Flisp Requisition

--- FlispCrm/ClassLibrary1/Pages/RequisitionPage.cs
+++ FlispCrm/ClassLibrary1/Pages/RequisitionPage.cs
@@ -12,11 +12,13 @@ namespace FlispCRM.Pages
 
         public override void BuildContents()
         {
+
             string persId = Dispatch.EitherField("key2");
+            string loanId = Dispatch.EitherField("key7");
 
-            //string requisitionUrl = "http://gertdevvm/ReportServer/Pages/ReportViewer.aspx?%2fRequisition&PersonId=" + persId;
+            //string requisitionUrl = "http://gertdevvm/ReportServer/Pages/ReportViewer.aspx?%2fRequisition&PersonId=" + persId + "&LoanId=" + loanId;
 
-            string requisitionUrl = "http://NHFCHP380FLISP/ReportServer/Pages/ReportViewer.aspx?%2fRequisition&PersonId=" + persId;
+            string requisitionUrl = $@"http://NHFCHP380FLISP/ReportServer/Pages/ReportViewer.aspx?%2fRequisition&PersonId=" + persId + "&LoanId=" + loanId;
 
             AddContent($@"<iframe src='{requisitionUrl}' id=Requisition style='border:none; overflow:auto; width: 100%; height: 1000px;'></iframe>");
 
